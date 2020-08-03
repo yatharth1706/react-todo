e0b8a0d5b863fc992b53023e1eab3562f0b3392d
Yatharth <yatharthverma070@gmail.com>
2020-08-03 16:36:06 +0530

    Fix bugs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoContainer from './components/TodoContainer';
 import LeftSidebar from './components/LeftSidebar';
+import RightSidebar from './components/RightSidebar';
 
 function App() {
+  const [toShow, setToShow] = useState('welcome');
+
   return (
     <div className="App">
       <h2>Todo App</h2>
-      <TodoContainer left = {<LeftSidebar />}/>
+      <TodoContainer left = {<LeftSidebar />} right = {<RightSidebar toshow={toShow} />}/>
     </div>
   );
 }
